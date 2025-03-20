16f4ec1fa47f7eeb4c758682c28b43507460b510
CocococoCY <corent1.lebris@gmail.com>
2025-03-20 21:36:16 +0100

    ContratDePartenariat.java

--- src/modele/Contrat.java
+++ src/modele/Contrat.java
@@ -2,61 +2,16 @@ package modele;
 
 import java.util.Date;
 
-public class Contrat {
-    private int id;
-    private int idCommerce;
-    private int idCentreTri;
+public class ContratDePartenariat {
     private Date dateDebut;
     private Date dateFin;
+    private String reglesUtilisation;
+    private int id;
 
-    public Contrat(int id, int idCommerce, int idCentreTri, Date dateDebut, Date dateFin) {
+    public ContratDePartenariat(Date debut, Date fin, String regles, int id) {
+        this.dateDebut = debut;
+        this.dateFin = fin;
+        this.reglesUtilisation = regles;
         this.id = id;
-        this.idCommerce = idCommerce;
-        this.idCentreTri = idCentreTri;
-        this.dateDebut = dateDebut;
-        this.dateFin = dateFin;
-    }
-
-    // Getters et Setters
-
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public int getIdCommerce() {
-        return idCommerce;
-    }
-
-    public void setIdCommerce(int idCommerce) {
-        this.idCommerce = idCommerce;
-    }
-
-    public int getIdCentreTri() {
-        return idCentreTri;
-    }
-
-    public void setIdCentreTri(int idCentreTri) {
-        this.idCentreTri = idCentreTri;
-    }
-
-    public Date getDateDebut() {
-        return dateDebut;
-    }
-
-    public void setDateDebut(Date dateDebut) {
-        this.dateDebut = dateDebut;
-    }
-
-    public Date getDateFin() {
-        return dateFin;
-    }
-
-    public void setDateFin(Date dateFin) {
-        this.dateFin = dateFin;
     }
 }
