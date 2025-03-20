c6b718e635b3ce61ae54c7325560dfa3ec0f1321
CocococoCY <corent1.lebris@gmail.com>
2025-03-20 21:34:16 +0100

    Commerce.java

--- src/modele/Commerce.java
+++ src/modele/Commerce.java
@@ -1,4 +1,17 @@
 package modele;
 
 public class Commerce {
+    private String nom;
+    private int id;
+    private ContratDePartenariat contrat;
+
+    public Commerce(String nom, int id, ContratDePartenariat contrat) {
+        this.nom = nom;
+        this.id = id;
+        this.contrat = contrat;
+    }
+
+    public void appliquerReduction(Menage menage, int points) {
+        System.out.println("Réduction appliquée de " + points + " points pour " + menage.consulterPointsFidelite() + " points.");
+    }
 }
