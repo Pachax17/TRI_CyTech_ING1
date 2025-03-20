57e84cf1ec789abb7558c1dfc36611e564d40c8d
CocococoCY <corent1.lebris@gmail.com>
2025-03-20 21:40:46 +0100

    Depot.java

--- src/modele/Depot.java
+++ src/modele/Depot.java
@@ -3,80 +3,31 @@ package modele;
 import java.util.Date;
 
 public class Depot {
-    private int id;
-    private Menage menage;
-    private Poubelle poubelle;
-    private Date dateDepot;
-    private String typeDechet;
-    private double quantite;
-    private int pointsGagnes;
+    private int idPoubelle;
+    private Date heureDepot;
+    private TypeDechets dechets;
+    private int quantiteDechets;
+    private int pointsParDepot;
 
-    public Depot(int id, Menage menage, Poubelle poubelle, Date dateDepot, String typeDechet, double quantite, int pointsGagnes) {
-        this.id = id;
-        this.menage = menage;
-        this.poubelle = poubelle;
-        this.dateDepot = dateDepot;
-        this.typeDechet = typeDechet;
-        this.quantite = quantite;
-        this.pointsGagnes = pointsGagnes;
+    public Depot(int idPoubelle, Date heureDepot, TypeDechets dechets, int quantiteDechets) {
+        this.idPoubelle = idPoubelle;
+        this.heureDepot = heureDepot;
+        this.dechets = dechets;
+        this.quantiteDechets = quantiteDechets;
+        this.pointsParDepot = calculerPoidsTotal();
     }
 
-    // Getters et setters
-
-
-    public int getId() {
-        return id;
+    public void ajouterDechet(TypeDechets dechet, int quantite) {
+        this.dechets = dechet;
+        this.quantiteDechets += quantite;
+        this.pointsParDepot = calculerPoidsTotal();
     }
 
-    public void setId(int id) {
-        this.id = id;
+    public int calculerPoidsTotal() {
+        return this.dechets.getPoids() * this.quantiteDechets;
     }
 
-    public Menage getMenage() {
-        return menage;
-    }
-
-    public void setMenage(Menage menage) {
-        this.menage = menage;
-    }
-
-    public Poubelle getPoubelle() {
-        return poubelle;
-    }
-
-    public void setPoubelle(Poubelle poubelle) {
-        this.poubelle = poubelle;
-    }
-
-    public Date getDateDepot() {
-        return dateDepot;
-    }
-
-    public void setDateDepot(Date dateDepot) {
-        this.dateDepot = dateDepot;
-    }
-
-    public String getTypeDechet() {
-        return typeDechet;
-    }
-
-    public void setTypeDechet(String typeDechet) {
-        this.typeDechet = typeDechet;
-    }
-
-    public double getQuantite() {
-        return quantite;
-    }
-
-    public void setQuantite(double quantite) {
-        this.quantite = quantite;
-    }
-
-    public int getPointsGagnes() {
-        return pointsGagnes;
-    }
-
-    public void setPointsGagnes(int pointsGagnes) {
-        this.pointsGagnes = pointsGagnes;
+    public int getPointsParDepot() {
+        return pointsParDepot;
     }
 }
