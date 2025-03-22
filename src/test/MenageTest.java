8c09c612f504f8f8cf93fd1946ba9e350c2e1ad8
CocococoCY <corent1.lebris@gmail.com>
2025-03-23 00:38:30 +0100

    Update MenageTest.java

--- src/test/MenageTest.java
+++ src/test/MenageTest.java
@@ -1,4 +1,27 @@
 package test;
 
+import modele.*;
+
+import java.util.Date;
+
 public class MenageTest {
+    public static void main(String[] args) {
+        Menage menage = new Menage(1, "Durand", "XYZ123");
+
+        PoubelleIntelligente poubelleJaune = new PoubelleIntelligente(1, 100, TypePoubelle.JAUNE, 48.8566, 2.3522);
+        Depot depotPlastique = new Depot(101, new Date(), TypeDechets.PLASTIQUE, 3);
+
+        // Compatible : JAUNE <-> PLASTIQUE
+        menage.deposerDechets(poubelleJaune, depotPlastique);
+
+        System.out.println("Points acquis : " + menage.consulterPointsFidelite()); // devrait être 3
+
+        PoubelleIntelligente poubelleVerte = new PoubelleIntelligente(2, 100, TypePoubelle.VERTE, 48.8566, 2.3522);
+        Depot depotPapier = new Depot(102, new Date(), TypeDechets.PAPIER, 2);
+
+        // Incompatible : VERTE != BLEUE
+        menage.deposerDechets(poubelleVerte, depotPapier);
+
+        System.out.println("Points après second dépôt : " + menage.consulterPointsFidelite()); // doit rester 3
+    }
 }
