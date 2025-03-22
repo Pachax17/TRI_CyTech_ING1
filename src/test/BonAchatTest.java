c0743dc12323d987ad7c1258644c56155b605ea4
CocococoCY <corent1.lebris@gmail.com>
2025-03-23 00:36:11 +0100

    Update BonAchatTest.java

--- src/test/BonAchatTest.java
+++ src/test/BonAchatTest.java
@@ -1,4 +1,10 @@
 package test;
 
+import modele.BonAchat;
+
 public class BonAchatTest {
+    public static void main(String[] args) {
+        BonAchat bon = new BonAchat(5);
+        bon.creerBonAchat(); // Affiche le message de création
+    }
 }
