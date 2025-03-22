102c34d548090834804fd768c7b519b8da04f6e5
CocococoCY <corent1.lebris@gmail.com>
2025-03-23 00:30:22 +0100

    Update BonAchatDAO.java

--- src/dao/BonAchatDAO.java
+++ src/dao/BonAchatDAO.java
@@ -1,4 +1,23 @@
 package dao;
 
+import modele.BonAchat;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class BonAchatDAO {
+    private Connection connection;
+
+    public BonAchatDAO(Connection connection) {
+        this.connection = connection;
+    }
+
+    public void insertBonAchat(BonAchat bon) throws SQLException {
+        String sql = "INSERT INTO bon_achat (valeur) VALUES (?)";
+        try (PreparedStatement stmt = connection.prepareStatement(sql)) {
+            stmt.setInt(1, bon.getValeur());
+            stmt.executeUpdate();
+        }
+    }
 }
